7e058e2b1d2b2aa5c2ecbd93be644c3dff9a69d0
@yuansirsi <1506643177@qq.com>
2022-06-19 14:35:45 +0800

    Parking功能开发

---
+++ src/main/java/com/yx/service/impl/ParkingServiceImpl.java
@@ -0,0 +1,10 @@
+package com.yx.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.yx.dao.ParkingMapper;
+import com.yx.model.Parking;
+import com.yx.service.ParkingService;
+
+public class ParkingServiceImpl extends ServiceImpl<ParkingMapper, Parking> implements ParkingService{
+
+}
