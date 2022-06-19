763852e514ddac2d78a0f2d928546880ab3725cf
34646 <34646@DESKTOP-RV1KRUQ>
2022-06-19 23:18:46 +0800

    用户信息管理的增删改查

--- src/main/java/com/yx/controller/UserinfoController.java
+++ src/main/java/com/yx/controller/UserinfoController.java
@@ -2,11 +2,15 @@ package com.yx.controller;
 
 
 import com.yx.util.JsonObject;
+import com.yx.util.R;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
+
+import java.util.Arrays;
+import java.util.List;
 
 import javax.annotation.Resource;
 
@@ -15,6 +19,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -60,5 +65,41 @@ public class UserinfoController {
     public IPage<Userinfo> findListByPage(@RequestParam Integer page,
                                           @RequestParam Integer pageCount){
         return userinfoService.findListByPage(page, pageCount);
+    }
+	
+	@ApiOperation(value = "新增")
+    @RequestMapping("/add")
+	public R add(@RequestBody Userinfo userinfo) {
+		userinfoService.add(userinfo);
+        return R.ok();
+	}
+	
+	@ApiOperation(value = "删除")
+	@RequestMapping("/deleteByIds")
+	public R delete(String ids) {
+		//先转成集合对象
+		List<String> list= Arrays.asList(ids.split(","));
+		
+		//遍历删除
+		for(String id:list) {
+			//string类型转换成Long
+			userinfoService.delete(Long.parseLong(id));
+		}
+		return R.ok();
+		
+	}
+	
+    @ApiOperation(value = "修改")
+    @RequestMapping("/update")
+    public R update(String oldPwd,String newPwd,Integer id){
+        //根据id获取当前的数据记录
+        Userinfo user=userinfoService.findById(new Long(id));
+        if(oldPwd.equals(user.getPassword())){//前端输入的旧密码和数据库原密码一致
+            user.setPassword(newPwd);
+            userinfoService.updateData(user);
+            return R.ok();
+        }else{
+            return R.fail("旧密码输入错误！");
+        }
     }
 }
