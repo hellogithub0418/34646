239ac0d0746e2ab1a47e5709c21a1d6bbb79f4ea
ASUS <ASUS@LAPTOP-INKIEM4O>
2022-06-23 11:39:43 +0800

    House模块

--- src/main/java/com/yx/model/House.java
+++ src/main/java/com/yx/model/House.java
@@ -5,25 +5,45 @@ import java.util.Date;
 
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import io.swagger.annotations.ApiModel;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
 
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author cuijianhui
+ * @since 2020-12-08
+ */
 @Data
 @EqualsAndHashCode(callSuper = false)
 @Accessors(chain = true)
-@ApiModel(value = "House对象",description ="" )
-public class House implements Serializable{
-	/**
-	 * 
-	 */
-	private Integer houseId;
-	private String numbers;
-	private Integer status;
-	private Date intoDate;
-	private Integer buildingId;
-	private String remarks;
-	private Double area;
+@ApiModel(value="House对象", description="")
+public class House implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer houseId;
+
+    private String numbers;
+
+    private Integer status;
+
+    @JsonFormat(locale="zh", timezone="GMT+8", pattern="yyyy-MM-dd")
+    private Date intoDate;
+
+    private Integer buildingId;
+
+    private String remarks;
+
+    private Double area;
+
+
+
 }
